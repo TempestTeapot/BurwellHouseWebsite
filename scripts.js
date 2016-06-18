7505dc483d8c52622e25cc7c2096007bd5715877
Carla Hennes <carla@Peters-Mac-mini-2.local>
2016-06-18 10:19:51 -0500

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var tagElement = document.getElementsByTagName('li');
+var classElement = document.getElementsByClassName('content');
+
+for (var i = 0; i < tagElement.length; i++) {
+	if (classElement = "content") {
+			console.log(tagElement[i].innerHTML);
+	};
+
+};
+
+console.log(classElement);
